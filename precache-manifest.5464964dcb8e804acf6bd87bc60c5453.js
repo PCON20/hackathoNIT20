7eafa8dabbe64e50741950216a4e89557f3f331f
Vivek Sharma <2017ugcs071@nitjsr.ac.in>
2019-11-29 14:35:51 +0530

    Updates

---
+++ precache-manifest.5464964dcb8e804acf6bd87bc60c5453.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c001cd44d09e7ce0619f410d1d0a91",
+    "url": "/hackathoNIT20/index.html"
+  },
+  {
+    "revision": "8f77200b99dd943fe53e",
+    "url": "/hackathoNIT20/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "3651457e950ee1bdfe4f",
+    "url": "/hackathoNIT20/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "8f77200b99dd943fe53e",
+    "url": "/hackathoNIT20/static/js/2.9364fad8.chunk.js"
+  },
+  {
+    "revision": "3651457e950ee1bdfe4f",
+    "url": "/hackathoNIT20/static/js/main.d0baa14a.chunk.js"
+  },
+  {
+    "revision": "e61070fd8453a04acbf6",
+    "url": "/hackathoNIT20/static/js/runtime~main.3529ac1a.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/hackathoNIT20/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/hackathoNIT20/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/hackathoNIT20/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/hackathoNIT20/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/hackathoNIT20/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/hackathoNIT20/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/hackathoNIT20/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/hackathoNIT20/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/hackathoNIT20/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/hackathoNIT20/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/hackathoNIT20/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/hackathoNIT20/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/hackathoNIT20/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/hackathoNIT20/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/hackathoNIT20/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/hackathoNIT20/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/hackathoNIT20/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/hackathoNIT20/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/hackathoNIT20/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/hackathoNIT20/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/hackathoNIT20/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/hackathoNIT20/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
